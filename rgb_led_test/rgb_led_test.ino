05e45f1b6a9cf737274ffb23345f8cc0b0c85123
Harrison Allen <harrisonallen555@gmail.com>
2023-03-30 00:04:59 -0400

    test script for the rgb led

---
+++ rgb_led_test/rgb_led_test.ino
@@ -0,0 +1,34 @@
+const int RED_PIN = 25;    // RED pin of the LED to PWM pin 4
+const int GREEN_PIN = 26 ;  // GREEN pin of the LED to PWM pin 5
+const int BLUE_PIN = 27;   // BLUE pin of the LED to PWM pin 6
+const int LED_UPDATE_RATE = 20;
+int brightness = 0; // LED brightness
+int increment = 5;  // brightness increment
+long timer;
+
+void setup()
+{
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+  Serial.begin(115200);
+
+  timer = millis();
+}
+
+void loop()
+{
+  if ((millis() - timer > LED_UPDATE_RATE)) {
+    brightness = brightness + increment;  // increment brightness for next loop iteration
+
+    if (brightness <= 0 || brightness >= 255)    // reverse the direction of the fading
+    {
+      increment = -increment;
+    }
+    brightness = constrain(brightness, 0, 255);
+    analogWrite(RED_PIN, brightness);
+    analogWrite(GREEN_PIN, brightness);
+    analogWrite(BLUE_PIN, brightness);
+    timer = millis();
+  }
+}
