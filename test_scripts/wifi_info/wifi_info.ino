3cf2cbc494fd5f8615b56b1ccd262e763e39b142
Harrison Allen <harrisonallen555@gmail.com>
2023-04-01 08:27:29 -0400

    added basic wifi info script

---
+++ test_scripts/wifi_info/wifi_info.ino
@@ -0,0 +1,42 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+// Load Wi-Fi library
+#include <WiFi.h>
+
+// Replace with your network credentials
+const char* ssid     = "Cozy Cove";
+const char* password = "Prickly Mochi 1005";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+
+  // Print ESP MAC Address
+  Serial.println("MAC address: ");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
