59d534911ce1282c588e0bdafeb52b8b1eea8db0
Harrison Allen <harrisonallen555@gmail.com>
2023-04-06 16:25:48 -0400

    Added alarm, and updated web server

--- library_tests/all_components_test/all_components_test.ino
+++ library_tests/all_components_test/all_components_test.ino
@@ -8,6 +8,7 @@
 #include <Sound.h>
 #include <ClockController.h>
 #include <nOOds.h>
+#include <AlarmObject.h>
 #include <ESPAsyncWebSrv.h>
 #include "Adafruit_LEDBackpack.h"
 #include "7-Segment-ASCII_HEX.h"
@@ -27,6 +28,9 @@ NTPClient timeClient(ntpUDP);
 Adafruit_7segment clockDisplay = Adafruit_7segment();
 const int photocellPin = 36;
 ClockController clockController(&clockDisplay, photocellPin);
+
+// Initialize Alarm Object
+AlarmObject alarmObject = AlarmObject();
 
 // nOOds fiber
 const int NOODS_PIN = 2;
@@ -51,10 +55,43 @@ long trellisTimer;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
+void notFound(AsyncWebServerRequest *request) {
+    Serial.println("Uh oh, 404 error!");
+    request->send(SD, "/webserver/404.html", "text/html");
+}
+const char* PARAM_ALARM_INPUT = "alarm_input";
+const char* PARAM_ALARM_ENABLED = "alarm_enabled";
+
+// String processor for webpage
+char placeholderCharArray[15];
+bool alarmUpdated;
+String processor(const String& var){
+  if (var == "TIME_STRING"){
+    return clockController.generateDisplayTime();
+  } else if (var == "ALARM_HOUR"){
+    sprintf(placeholderCharArray, "%02d", alarmObject._hour);
+    return String(placeholderCharArray);
+  } else if (var == "ALARM_MINUTE"){
+    sprintf(placeholderCharArray, "%02d", alarmObject._minute);
+    return String(placeholderCharArray);
+  } else if (var == "ALARM_ENABLED") {
+    sprintf(placeholderCharArray, "%s", alarmObject._enabled ? "checked" : "");
+    return String(placeholderCharArray);
+  } else if (var == "ALARM_UPDATE") {
+      if (alarmUpdated) {
+          alarmUpdated = false;
+          return String("<p style=\"color:green\">Alarm successfully updated!</p>");
+      }
+      return String();
+  }
+  return String();
+}
 
 // Local sketch variables
 long wifiTimer;
 int hour, minute, second, day;
+int lastMinute;
+bool alarmPlaying;
 
 void setup() {
   // Initialize Serial Monitor
@@ -92,11 +129,35 @@ void setup() {
   Serial.println(WiFi.localIP());
   clockController.displayIP(WiFi.localIP());
 
+  // Initialize a NTPClient to get time
+  timeClient.begin();
+  timeClient.setTimeOffset(-14400); // GMT -4, hours -> seconds
+  fetchTime();
+  lastMinute = clockController.getMinute();
+
   // Serve Webpage
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SD, "/webserver/index.html", "text/html");
   });
+  // Receive an HTTP GET request at <ESP_IP>/alarm/get?alarm_input=<time>&alarm_enabled=<bool>
+  server.on("/alarm", HTTP_GET, [](AsyncWebServerRequest *request){
+    if (request->params() == 0) {
+        request->send(SD, "/webserver/alarm.html", "text/html", false, processor);
+    } else { 
+        if (request->hasParam(PARAM_ALARM_INPUT)) {
+            setAlarm(request->getParam(PARAM_ALARM_INPUT)->value());
+            if (request->hasParam(PARAM_ALARM_ENABLED)) {
+                setAlarmEnabled(request->getParam(PARAM_ALARM_ENABLED)->value());
+            } else {
+                setAlarmEnabled("false");
+            }
+            alarmUpdated = true;
+            request->redirect("/alarm");
+        }
+    }
+  });
   server.serveStatic("/", SD, "/webserver/");
+  server.onNotFound(notFound);
   server.begin();
 
   // Start sound
@@ -116,11 +177,6 @@ void setup() {
   }
   trellis.writeDisplay();
 
-  // Initialize a NTPClient to get time
-  timeClient.begin();
-  timeClient.setTimeOffset(-14400); // GMT -4, hours -> seconds
-  fetchTime();
-
   // nOOds
   noodsTimer = millis();
 }
@@ -130,6 +186,22 @@ void loop() {
   noodsLoop();
   trellisLoop();
   sound.loop();
+    if (clockController.needsTimeUpdate()) {
+        fetchTime();
+    }
+    if (clockController.getMinute() != lastMinute) {
+        Serial.printf("%d -> %d\n", clockController.getMinute(), lastMinute);
+        lastMinute = clockController.getMinute();
+        if (alarmPlaying) {
+            Serial.println("Snoozing alarm");
+            sound.stop();
+            alarmPlaying = false;
+            setAlarm(alarmObject._snoozeDuration);
+        }
+        if (alarmObject._enabled && alarmObject.checkTime(&clockController)) {
+            playAlarm();
+        }
+    }
 }
 
 void noodsLoop() {
@@ -181,4 +253,49 @@ void fetchTime() {
         Serial.println("Failed to fetch time");
         clockController.ignoreTimeUpdate();
     }
+}
+
+void setAlarm(String alarmString) {
+    Serial.print("Alarm received: ");
+    Serial.println(alarmString);
+
+    int alarmHour, alarmMinute;
+    alarmHour = alarmString.substring(0, 2).toInt();
+    alarmMinute = alarmString.substring(3).toInt();
+    alarmObject.setTime(alarmHour, alarmMinute);
+    Serial.printf("Alarm set for %d:%d\n", alarmHour, alarmMinute);
+}
+
+void setAlarm(int minuteOffset) {
+    int alarmMinute = clockController.getMinute() + minuteOffset;
+    int alarmHour = clockController.getHour();
+    if (alarmMinute >= 60) {
+        alarmMinute -= 60;
+        alarmHour += 1;
+        if (alarmHour > 23) {
+            alarmHour = 0;
+        }
+    }
+    alarmObject.setTime(alarmHour, alarmMinute);
+    alarmObject.setEnabled(true);
+    Serial.printf("Alarm set for %d:%d\n", alarmHour, alarmMinute);
+}
+
+void setAlarmEnabled(String alarmEnabledString) {
+    Serial.print("Alarm enable status: ");
+    if (alarmEnabledString == "true") {
+        Serial.println("Enabled");
+    } else {
+        Serial.println("Disabled");
+    }
+    alarmObject.setEnabled(alarmEnabledString == "true");
+}
+
+void playAlarm() {
+    Serial.println("Alarm triggered!");
+    sound.setSoundFile(alarmObject._alarmFilename);
+    sound.setRepeating(true);
+    sound.setVolume(1);
+    sound.play();
+    alarmPlaying = true;
 }
